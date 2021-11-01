8ea00ae14884673084e37908abc845979d21c543
koreadinosaur <khj7741@gmail.com>
2021-11-02 01:19:41 +0900

    weather update

--- js/weather.js
+++ js/weather.js
@@ -2,7 +2,7 @@ const API_KEY = "42989711d4460f2284dadd91731935a6"; //openweathermap 에서 로�
 function geoSucess(position){
     const lat = position.coords.latitude; //위도
     const lon = position.coords.longitude; //경도
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     //openweathermap에서 API -> current weather Data항목에서 api doc클릭
     //-> By geographic coordinates에서 API call을 복사해서 url에 붙여넣는다.
     //거기다 내 위도, 경도, api key 입력하면 내 위치정보 뜬다.
